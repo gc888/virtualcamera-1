f1eab9a0f1f21d2cd4796fcdc4c3c8e28919ec73
Bartosz Strachowski <bstrachowski@gmail.com>
2019-02-24 14:07:07 +0100

    Framework of application add with canvas and buttons

---
+++ VirtualCamera/VirtualCamera/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VirtualCamera
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            Line myLine = new Line();
+            myLine.X1 = 100;
+            myLine.X2 = 500;
+            myLine.Y1 = 100;
+            myLine.Y2 = 500;
+            myLine.Stroke = Brushes.White;
+            MyCanvas.Children.Add(myLine);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Line myLine = new Line();
+            myLine.X1 = 200;
+            myLine.X2 = 300;
+            myLine.Y1 = 200;
+            myLine.Y2 = 600;
+            myLine.Stroke = Brushes.White;
+            MyCanvas.Children.Add(myLine);
+        }
+    }
+}
